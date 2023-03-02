1bddab49169555731b9932e5c1676d5b2078a676
Phil Eaton <phil@eatonphil.com>
2023-03-02 12:40:12 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     match std::env::var("MY_SECRET") {
         Ok(secret) => {
             println!("secret is: {} bytes long", secret.len());
+            // clarify stuff
             println!("secret is: {}", secret);
         },
         Err(_) => println!("no secret"),
