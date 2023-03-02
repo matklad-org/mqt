8d7886df8935d56b87d2506e2e2ea05eac6746aa
Aleksey Kladov <aleksey.kladov@gmail.com>
2023-03-02 17:22:12 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     match std::env::var("MY_SECRET") {
         Ok(secret) => {
-            println!("secret {} bytes long", secret.len());
+            println!("secret is {} bytes long", secret.len());
             println!("secret is: {}", secret);
         },
         Err(_) => println!("no secret"),
